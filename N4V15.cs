d4dda9127bab17885395514d168998d8fd1e3b2e
egorufimtsev <114737005+egorufimtsev@users.noreply.github.com>
2022-12-28 19:39:42 +0500

    Add files via upload

---
+++ N4V15.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal static class N4V15
+    {
+        private static void Main()
+        {
+            Console.Write("x = ");
+            var x = double.Parse(Console.ReadLine()!);
+            if (x is 0 or < -1)
+                Console.WriteLine("Некорректное число");
+            else
+            {
+                var y = Math.Sin((3.2 + Math.Sqrt(x + 1))/Math.Abs(5*x));
+                Console.WriteLine($"y = {y}");
+            }
+        }
+    }
+}
